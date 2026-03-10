e052b80a66eac85025701fbe8e1ac7dea79a71e7
Laura Machado <lauramachadosscontato@gamil.com>
2026-03-10 15:21:09 -0300

    Estrutura projeto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Album albumHarry = new Album();
+albumHarry.Nome = "Harry`s House";
+
+Musica musica1 = new Musica();
+musica1.Nome = "Matilda";
+
+Musica musica2 = new Musica();
+musica2.Nome = "As It Was";
+
+Musica musica3 = new Musica();
+musica3.Nome = "Little Freak";
+
+albumHarry.AdicionarMusica(musica1);
+albumHarry.AdicionarMusica(musica2);
+albumHarry.AdicionarMusica(musica3);
+
+albumHarry.ExibirDiscografia();
